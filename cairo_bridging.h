8070f8ebddbb7c0e74406f50bd3253cb61eafe0e
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-30 16:50:41 +1000

    Remove structs that weren't really private

--- cairo_bridging.h
+++ cairo_bridging.h
@@ -1,14 +1,8 @@
 struct _cairo_script_interpreter {};
-struct _cairo_script_interpreter_hooks {};
 struct _cairo {};
 struct _cairo_surface {};
 struct _cairo_device {};
-struct _cairo_matrix {};
 struct _cairo_pattern {};
-struct _cairo_user_data_key {};
-struct _cairo_rectangle_int {};
-struct _cairo_rectangle {};
-struct _cairo_rectangle_list {};
 struct _cairo_scaled_font {};
 struct _cairo_font_face {};
 struct _cairo_font_options {};
